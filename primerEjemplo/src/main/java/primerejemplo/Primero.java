ba0904a1058b1377852af6f5b73c1788c860848a
user <user@Dpt-Info-Carmen>
2017-10-09 11:11:17 +0200

    Merge origin/master
    Conflicts:
    	primerEjemplo/src/main/webapp/index.html

--- primerEjemplo/src/main/java/primerejemplo/Primero.java
+++ primerEjemplo/src/main/java/primerejemplo/Primero.java
@@ -11,6 +11,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import utils.Constantes;
 
 /**
  *
@@ -30,10 +31,19 @@ public class Primero extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
+        String paginaDestino = Constantes.PAGINA_INDEX;
+        for(String nombre: request.getParameterMap().keySet())
+        {
+            if (nombre.contains("r"))
+            {
+               paginaDestino = Constantes.PAGINA_ERROR; 
+            }  
+        }
         
-        
-        request.getRequestDispatcher("index.jsp").forward(request, response);
+     
+        request.getRequestDispatcher(paginaDestino).forward(request, response);
+ 
+            
 
     }
 
---
+++ primerEjemplo/src/main/webapp/error.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : error
+    Created on : 09-oct-2017, 11:04:00
+    Author     : user
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Se ha producido un error</h1>
+    </body>
+</html>
