9926ffee19df135a2f2fbfaf79aafbcb118a2e2e
user <user@Dpt-Info-Carmen>
2017-10-09 11:10:49 +0200

    Merge origin/master
    Conflicts:
    	primerEjemplo/src/main/webapp/index.html

---
+++ primerEjemplo/src/main/java/utils/Constantes.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+/**
+ *
+ * @author user
+ */
+public class Constantes {
+
+    public static final String PAGINA_INDEX = "index.jsp";
+
+    public static final String PAGINA_ERROR = "error.jsp";
+
+}
