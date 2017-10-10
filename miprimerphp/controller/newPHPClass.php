f80b3faed70f4e7d89e7b3f7127d1848f125a1ce
user <user@Dpt-Info-Carmen>
2017-10-10 09:21:42 +0200

    Merge origin/master
    Conflicts:
    	primerEjemplo/src/main/webapp/index.html

--- miprimerphp/controller/newPHPClass.php
+++ miprimerphp/controller/newPHPClass.php
@@ -1,15 +1,16 @@
 <?php
 
-namespace controllers;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-echo "antes";
-$kk = "2";
-include "./newEmptyPHPWebPage.php";
-?>
-
+/**
+ * Description of newPHPClass
+ *
+ * @author user
+ */
+class newPHPClass {
+    //put your code here
+}
