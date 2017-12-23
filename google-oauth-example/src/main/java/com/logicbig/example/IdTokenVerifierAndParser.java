fef561ba0dadb83587bda0cdb8e040f08ba49634
oscar <oscar@DESKTOP-E8BOLD9>
2017-12-23 22:37:12 +0100

    Merge origin/master
    Conflicts:
    	HTML5Application/public_html/contentindex.html

---
+++ google-oauth-example/src/main/java/com/logicbig/example/IdTokenVerifierAndParser.java
@@ -0,0 +1,31 @@
+package com.logicbig.example;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson.JacksonFactory;
+
+public class IdTokenVerifierAndParser {
+    private static final String GOOGLE_CLIENT_ID = "1027953223368-th3hh3onqnevoft8sgliam4kvntmtrvj.apps.googleusercontent.com";
+
+    public static GoogleIdToken.Payload getPayload (String tokenString) throws Exception {
+
+        JacksonFactory jacksonFactory = new JacksonFactory();
+        GoogleIdTokenVerifier googleIdTokenVerifier =
+                            new GoogleIdTokenVerifier(new NetHttpTransport(), jacksonFactory);
+
+        GoogleIdToken token = GoogleIdToken.parse(jacksonFactory, tokenString);
+
+        if (googleIdTokenVerifier.verify(token)) {
+            GoogleIdToken.Payload payload = token.getPayload();
+            if (!GOOGLE_CLIENT_ID.equals(payload.getAudience())) {
+                throw new IllegalArgumentException("Audience mismatch");
+            } else if (!GOOGLE_CLIENT_ID.equals(payload.getAuthorizedParty())) {
+                throw new IllegalArgumentException("Client ID mismatch");
+            }
+            return payload;
+        } else {
+            throw new IllegalArgumentException("id token cannot be verified");
+        }
+    }
+}
