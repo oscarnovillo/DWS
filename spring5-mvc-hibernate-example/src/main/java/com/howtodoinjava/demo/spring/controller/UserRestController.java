b8f69de4cbd088d6de90b1c8a895b918a6c55018
oscar <oscar@DESKTOP-E8BOLD9>
2018-01-05 14:11:34 +0100

    Merge origin/master
    Conflicts:
    	HTML5Application/public_html/contentindex.html

---
+++ spring5-mvc-hibernate-example/src/main/java/com/howtodoinjava/demo/spring/controller/UserRestController.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.howtodoinjava.demo.spring.controller;
+
+import com.howtodoinjava.demo.spring.model.User;
+import com.howtodoinjava.demo.spring.service.UserService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import static org.springframework.web.bind.annotation.RequestMethod.PUT;
+
+/**
+ *
+ * @author oscar
+ */
+@RestController
+@RequestMapping("/user")
+public class UserRestController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(method = GET)
+    public List<User> list() {
+        return userService.list();
+    }
+
+    @RequestMapping(value = "/{id}", method = GET)
+    public Object get(@PathVariable String id) {
+        return new User(1L,"test","test");
+    }
+
+    @RequestMapping(value = "/{id}", method = PUT)
+    public ResponseEntity<?> put(@PathVariable String id, @RequestBody Object input) {
+        return null;
+    }
+
+    @RequestMapping(value = "/{id}", method = POST)
+    public ResponseEntity<?> post(@PathVariable String id, @RequestBody Object input) {
+        return null;
+    }
+
+    @RequestMapping(value = "/{id}", method = DELETE)
+    public ResponseEntity<Object> delete(@PathVariable String id) {
+        return null;
+    }
+
+}
