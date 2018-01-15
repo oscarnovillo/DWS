4f20252fdbfadc3dba5daad79a8e2f269d23a470
oscar <oscar@DESKTOP-E8BOLD9>
2018-01-15 21:09:06 +0100

    Merge origin/master
    Conflicts:
    	HTML5Application/public_html/contentindex.html

--- baseDatos/src/main/java/servlets/GoogleHttpConsumerFootball.java
+++ baseDatos/src/main/java/servlets/GoogleHttpConsumerFootball.java
@@ -5,7 +5,6 @@
  */
 package servlets;
 
-import com.google.api.client.http.EmptyContent;
 import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestFactory;
@@ -13,15 +12,19 @@ import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.UrlEncodedContent;
 import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.GenericJson;
+import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.JsonObjectParser;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.ArrayMap;
 import com.google.api.client.util.GenericData;
+import com.google.gson.reflect.TypeToken;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+
+import java.lang.reflect.Type;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -33,8 +36,8 @@ import model.Arrives;
  *
  * @author oscar
  */
-@WebServlet(name = "GoogleHttpConsumingApi", urlPatterns = {"/api"})
-public class GoogleHttpConsumingApi extends HttpServlet {
+@WebServlet(name = "GoogleHttpConsumerFootball", urlPatterns = {"/apiF"})
+public class GoogleHttpConsumerFootball extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -46,9 +49,8 @@ public class GoogleHttpConsumingApi extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-
-        HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+            throws ServletException, IOException {
+          HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
         JsonFactory JSON_FACTORY = new JacksonFactory();
         HttpRequestFactory requestFactory
           = HTTP_TRANSPORT.createRequestFactory(new HttpRequestInitializer() {
@@ -58,66 +60,23 @@ public class GoogleHttpConsumingApi extends HttpServlet {
               }
           });
 
-        GenericUrl url = new GenericUrl("https://openbus.emtmadrid.es:9443/emt-proxy-server/last/geo/GetArriveStop.php");
-
-        GenericData data = new GenericData();
-        data.put("idClient", "WEB.SERV.oscar.novillo@iesquevedo.es");
-        data.put("passKey", "4C7A2AC7-2AC4-4AAE-9E63-E27EEA72969E");
-        data.put("idStop", "3727");
-
-        HttpRequest requestGoogle = requestFactory.buildPostRequest(url, new UrlEncodedContent(data));
-        requestGoogle.getHeaders().set("X-Auth-Token", "2deee83e549c4a6e9709871d0fd58a0a");
-        url.set("idClient", "WEB.SERV.oscar.novillo@iesquevedo.es");
-        url.set("passKey", "4C7A2AC7-2AC4-4AAE-9E63-E27EEA72969E");
-        url.set("idStop", "3727");
-
-        //url = new GenericUrl("http://api.football-data.org/v1/teams/745/players");
-        Arrives arr = requestGoogle.execute().parseAs(Arrives.class);
-        GenericJson json = requestGoogle.execute().parseAs(GenericJson.class);
-        //response.getWriter().print(arr.getArrives().size());
-
-        ArrayList arrives = (ArrayList) json.get("arrives");
-        response.getWriter().print("<html><body>");
-        for (int i = 0; i < arrives.size(); i++) {
-            ArrayMap arrive = (ArrayMap)arrives.get(i);
-            response.getWriter().print(arrive.get("busTimeLeft")+" ");
-            response.getWriter().print(arrive.get("lineId")+" ");
-            response.getWriter().print(arrive.get("busDistance")+" ");
-            response.getWriter().print("<br>");
-        }
-        
-        data = new GenericData();
-        data.put("idClient", "WEB.SERV.oscar.novillo@iesquevedo.es");
-        data.put("passKey", "4C7A2AC7-2AC4-4AAE-9E63-E27EEA72969E");
-        data.put("line", "76");
-        data.put("direction", "PLAZA BEATA");
-        url = new GenericUrl("https://openbus.emtmadrid.es:9443/emt-proxy-server/last/geo/GetStopsLine.php");
-        requestGoogle = requestFactory.buildPostRequest(url, new UrlEncodedContent(data));
-        json = requestGoogle.execute().parseAs(GenericJson.class);
-        
-      ArrayList stops = (ArrayList) json.get("stop");
-      response.getWriter().print(json.get("destination"));
-      for (int i = 0; i < stops.size(); i++) {
-            ArrayMap stop = (ArrayMap)stops.get(i);
-            response.getWriter().print(stop.get("stopId")+" ");
-            response.getWriter().print(stop.get("name")+" ");
-           
-            response.getWriter().print("<br>");
-        }
       
       
-      
-      
+      response.getWriter().print("<html><body>");
 
 
-        url = new GenericUrl("http://api.football-data.org/v1/competitions/");
-        //url.set("season","2017");
+        GenericUrl url = new GenericUrl("http://api.football-data.org/v1/competitions/");
+        url.set("season","2017");
         //data.put("season", "2017");
-        requestGoogle = requestFactory.buildPostRequest(url,new UrlEncodedContent(data));
+        HttpRequest requestGoogle = requestFactory.buildGetRequest(url);
         requestGoogle.getHeaders().set("X-Auth-Token", "2deee83e549c4a6e9709871d0fd58a0a");
-        json = requestGoogle.execute().parseAs(GenericJson.class);
 
-        response.getWriter().print(json.toPrettyString());
+        
+        response.getWriter().print(requestGoogle.execute().parseAsString());
+        Type type = new TypeToken<List<GenericJson>>(){}.getType();
+        List<GenericJson> json = (List)requestGoogle.execute().parseAs(type);
+
+        response.getWriter().print(json.get(0).toPrettyString());
         
         
         
@@ -135,7 +94,7 @@ public class GoogleHttpConsumingApi extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
@@ -149,7 +108,7 @@ public class GoogleHttpConsumingApi extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
